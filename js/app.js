64d861a983169ab5c24ccfa3ef6efe283656be30
Nathan Schmidt <nschmdt@gmail.com>
2022-04-15 10:34:00 -0600

    Fixed bug when search was empty

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
 //on load of the document add event listener to the search input
 document.addEventListener("DOMContentLoaded", () => {
-    //Get search input element
-    const search = document.getElementById("search");
-    //Add Event Listener to the search input
-    search.addEventListener("keyup", (e) => {
-        //Get the search input value
-        const term = e.target.value.trim();
-        //if term is not empty search with term
-        if (term) {
-            //filter a tags based on search input
+	//Get search input element
+	const search = document.getElementById("search");
+	//Add Event Listener to the search input
+	search.addEventListener("keyup", (e) => {
+		if (e.target.value) {
+			//Get the search input value
+			const term = e.target.value.trim();
+			//if term is not empty search with term
+			if (term) {
+				//filter a tags based on search input
+				const links = document.querySelectorAll("a");
+				links.forEach((link) => {
+					let caption = link
+						.getAttribute("data-caption")
+						.toLowerCase();
+					if (caption.includes(term.toLowerCase())) {
+						link.style.display = "block";
+					} else {
+						link.style.display = "none";
+					}
+				});
+			}
+        } else {
+            //if term is empty show all links
             const links = document.querySelectorAll("a");
             links.forEach((link) => {
-                let caption = link.getAttribute("data-caption").toLowerCase();
-                if (caption.includes(term.toLowerCase())) {
-                    link.style.display = "block";
-                } else {
-                    link.style.display = "none";
-                }
+                link.style.display = "block";
             });
-        };
-    });
+        }
+	});
 });
-
