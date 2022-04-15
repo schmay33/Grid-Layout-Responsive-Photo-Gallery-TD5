439f8cd28bcacca698644aa02779855059d7b411
Nathan Schmidt <nschmdt@gmail.com>
2022-04-15 10:41:26 -0600

    Update listener

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 //on load of the document add event listener to the search input
 document.addEventListener("DOMContentLoaded", () => {
 	//Get search input element
-	const search = document.getElementById("search");
+    const search = document.getElementById("search");
+    //Get links
+    const links = document.querySelectorAll("a");
 	//Add Event Listener to the search input
 	search.addEventListener("keyup", (e) => {
 		if (e.target.value) {
@@ -9,8 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			const term = e.target.value.trim();
 			//if term is not empty search with term
 			if (term) {
-				//filter a tags based on search input
-				const links = document.querySelectorAll("a");
 				links.forEach((link) => {
 					let caption = link
 						.getAttribute("data-caption")
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			}
         } else {
             //if term is empty show all links
-            const links = document.querySelectorAll("a");
             links.forEach((link) => {
                 link.style.display = "block";
             });
